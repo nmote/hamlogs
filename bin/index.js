01f210f962583a9e70e30363276de9be6b3f7f61
Nat Mote <nat@natmote.net>
2021-03-09 08:27:17 -0800

    Move CLI logic to flow-typed and transpiled file

--- bin/index.js
+++ bin/index.js
@@ -2,10 +2,6 @@
 
 // TODO make this whole piece of software not shitty
 
-const {main} = require('../lib/run');
+const {main} = require('../lib/cli');
 
-const callsign = process.argv[2];
-const park = process.argv[3];
-const inputFile = process.argv[4];
-
-process.stdout.write(main(callsign, park, inputFile));
+main();
---
+++ src/cli.js
@@ -0,0 +1,11 @@
+// @flow strict
+
+import {main as run} from './run';
+
+export function main() {
+  const callsign = process.argv[2];
+  const park = process.argv[3];
+  const inputFile = process.argv[4];
+
+  process.stdout.write(run(callsign, park, inputFile));
+}
