b0c161bf18ecf2eeb16e1e21a9a6b4e9f4866e69
Nat Mote <nat@natmote.net>
2021-03-08 22:14:16 -0800

    Extract run function from run.js

--- src/run.js
+++ src/run.js
@@ -6,14 +6,18 @@ import {strict as invariant} from 'assert';
 import {parse} from './parse';
 import {toAdif} from './adif';
 
+function run(callsign: string, park: string, inputText: string): string {
+  // TODO validate callsign and park
+  const log = parse(inputText);
+  const adif = toAdif(callsign, park, log);
+  return adif;
+}
+
 export function main(callsign: ?string, park: ?string, inputFile: ?string): string {
   // TODO do proper validaton
   invariant(inputFile != null);
   invariant(callsign != null);
   invariant(park != null);
-  // TODO validate callsign and park
-  const file = fs.readFileSync(inputFile).toString();
-  const log = parse(file);
-  const adif = toAdif(callsign, park, log);
-  return adif;
+  const inputText = fs.readFileSync(inputFile).toString();
+  return run(callsign, park, inputText);
 }
