eb50c20cd639def1c69b30ce9c7b2dfa4bb81834
Nat Mote <nat@natmote.net>
2021-03-09 17:18:52 -0800

    Move CLI-specific logic to cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,22 @@
 // @flow strict
 
-import {main as run} from './index';
+import fs from 'fs';
+// $FlowFixMe[missing-export] this export does exist at runtime
+import {strict as invariant} from 'assert';
+
+import {CSVToAdif} from './index';
 
 export function main() {
   const callsign = process.argv[2];
   const park = process.argv[3];
   const inputFile = process.argv[4];
 
-  process.stdout.write(run(callsign, park, inputFile));
+  // TODO do proper validaton
+  invariant(callsign != null);
+  invariant(park != null);
+  invariant(inputFile != null);
+
+  const inputText = fs.readFileSync(inputFile).toString();
+
+  process.stdout.write(CSVToAdif(callsign, park, inputText));
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // @flow strict
 
-import fs from 'fs';
-// $FlowFixMe[missing-export] this export does exist at runtime
-import {strict as invariant} from 'assert';
 import {parse} from './parse';
 import {toAdif} from './adif';
 
@@ -11,13 +8,4 @@ export function CSVToAdif(callsign: string, park: string, inputText: string): st
   const log = parse(inputText);
   const adif = toAdif(callsign, park, log);
   return adif;
-}
-
-export function main(callsign: ?string, park: ?string, inputFile: ?string): string {
-  // TODO do proper validaton
-  invariant(inputFile != null);
-  invariant(callsign != null);
-  invariant(park != null);
-  const inputText = fs.readFileSync(inputFile).toString();
-  return CSVToAdif(callsign, park, inputText);
 }
