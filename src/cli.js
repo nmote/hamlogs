bcb0f1759a47f25a946990075c8257929b937657
Nat Mote <nat@natmote.net>
2021-03-09 17:41:42 -0800

    Add friendly error when reading the input file fails

--- src/cli.js
+++ src/cli.js
@@ -23,12 +23,22 @@ function assertArg(arg: ?string, msg: string): string {
   }
 }
 
-export function main() {
+export function main(): void {
   const callsign = assertArg(process.argv[2], "Please provide the operator's callsign");
   const park = assertArg(process.argv[3], 'Please provide your park identifier');
   const inputFile = assertArg(process.argv[4], 'Please provide the full path to your CSV file');
 
-  const inputText = fs.readFileSync(inputFile).toString();
+  let inputText;
+  try {
+    inputText = fs.readFileSync(inputFile).toString();
+  } catch (e) {
+    process.stderr.write(`Error while reading file: ${inputFile}\n`);
+    process.stderr.write(
+      'Please ensure that the file exists, and that you have typed the path correctly\n\n'
+    );
+    process.stderr.write(e.message + '\n');
+    return process.exit(1);
+  }
 
   process.stdout.write(CSVToAdif(callsign, park, inputText));
 }
