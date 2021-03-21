e4ad69c008b45a486471ef00af4a918666941326
Nat Mote <nat@natmote.net>
2021-03-20 21:00:53 -0700

    Add objectValues util

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+// @flow strict
+
+/** A more permissive signature for Object.values. This is unsound on class
+ * instances, and can lead to performance problems with type inference if a
+ * reasonable type argument is not provided. */
+export function objectValues<Values>(obj: {+[string]: Values}): Array<Values> {
+  // $FlowFixMe[unclear-type]
+  return (Object.values(obj): any);
+}
