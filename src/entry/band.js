663fbb5fc77ea90400adbe57c8e411ce4119b20c
Nat Mote <nat@natmote.net>
2022-02-22 13:50:57 -0800

    Add functioning SOTA band conversion for all bands

--- src/entry/band.js
+++ src/entry/band.js
@@ -26,10 +26,13 @@ class Range {
 class BandImpl {
   +band: string;
   +freqs: Range;
+  // Representative frequency for the band in MHz.
+  +sotaFreq: string;
 
-  constructor(band: string, freqs: Range) {
+  constructor(band: string, freqs: Range, sotaFreq: string) {
     this.band = band;
     this.freqs = freqs;
+    this.sotaFreq = sotaFreq;
   }
 
   toADIFBand(): string {
@@ -37,18 +40,7 @@ class BandImpl {
   }
 
   toSOTABand(): string {
-    // TODO support all bands
-    switch (this.toADIFBand()) {
-      case '40M':
-        return '7MHz';
-      case '20M':
-        return '14MHz';
-      default:
-        // I don't want to plumb a result through this function since it will be
-        // encapsulated with a band object soon, and this won't be possible. For
-        // now, the SOTA importer will have to catch this if it happens.
-        return 'UnsupportedBand';
-    }
+    return this.sotaFreq + 'MHz';
   }
 }
 
@@ -63,29 +55,38 @@ function normalizeBand(inputParam: string): string {
   return input.toLowerCase();
 }
 
-function makeHamBandEntry(name: string, freqs: Range): [string, BandImpl] {
-  return [name, new BandImpl(name, freqs)];
+function makeHamBandEntry(name: string, freqs: Range, sotaFreq: string): [string, BandImpl] {
+  return [name, new BandImpl(name, freqs, sotaFreq)];
 }
 
 // TODO add more ham bands
 // TODO check frequency ranges against international allocations
+// SOTA frequencies are taken from
+// https://www.sotadata.org.uk/en/upload/activator/csv/info except where noted.
 const hamBands: Map<string, BandImpl> = new Map([
-  makeHamBandEntry('2200m', new Range(0.1357, 0.1378)),
-  makeHamBandEntry('630m', new Range(0.472, 0.479)),
-  makeHamBandEntry('160m', new Range(1.8, 2.0)),
-  makeHamBandEntry('80m', new Range(3.5, 4.0)),
-  makeHamBandEntry('60m', new Range(5.3305, 5.4065)),
-  makeHamBandEntry('40m', new Range(7.0, 7.3)),
-  makeHamBandEntry('30m', new Range(10.1, 10.15)),
-  makeHamBandEntry('20m', new Range(14.0, 14.35)),
-  makeHamBandEntry('17m', new Range(18.068, 18.168)),
-  makeHamBandEntry('15m', new Range(21.0, 21.45)),
-  makeHamBandEntry('12m', new Range(24.89, 24.99)),
-  makeHamBandEntry('10m', new Range(28.0, 29.7)),
-  makeHamBandEntry('6m', new Range(50.0, 54.0)),
-  makeHamBandEntry('2m', new Range(144.0, 148.0)),
-  makeHamBandEntry('1.25m', new Range(219.0, 225.0)),
-  makeHamBandEntry('70cm', new Range(420.0, 450.0)),
+  // The SOTA page does not have an entry listed for this band, so I have chosen
+  // a representative frequency that is within it.
+  makeHamBandEntry('2200m', new Range(0.1357, 0.1378), '0.136'),
+  // The SOTA page does not have an entry listed for this band, so I have chosen
+  // a representative frequency that is within it.
+  makeHamBandEntry('630m', new Range(0.472, 0.479), '0.475'),
+  makeHamBandEntry('160m', new Range(1.8, 2.0), '1.8'),
+  makeHamBandEntry('80m', new Range(3.5, 4.0), '3.5'),
+  makeHamBandEntry('60m', new Range(5.3305, 5.4065), '5'),
+  makeHamBandEntry('40m', new Range(7.0, 7.3), '7'),
+  makeHamBandEntry('30m', new Range(10.1, 10.15), '10'),
+  makeHamBandEntry('20m', new Range(14.0, 14.35), '14'),
+  makeHamBandEntry('17m', new Range(18.068, 18.168), '18'),
+  makeHamBandEntry('15m', new Range(21.0, 21.45), '21'),
+  makeHamBandEntry('12m', new Range(24.89, 24.99), '24'),
+  makeHamBandEntry('10m', new Range(28.0, 29.7), '28'),
+  makeHamBandEntry('6m', new Range(50.0, 54.0), '50'),
+  makeHamBandEntry('2m', new Range(144.0, 148.0), '144'),
+  // The SOTA page does not have an entry listed for this band, so I have chosen
+  // a representative frequency that is within it.
+  makeHamBandEntry('1.25m', new Range(219.0, 225.0), '222'),
+  makeHamBandEntry('70cm', new Range(420.0, 450.0), '432'),
+  makeHamBandEntry('23cm', new Range(1240, 1300), '1240'),
 ]);
 
 export function parseBand(input: string): Result<Band, string> {
